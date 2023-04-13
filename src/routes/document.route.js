05346f2b91294e36164a3470130497ab1b3144c4
Jéssica <jessicah.dias23@hotmail.com>
2023-04-13 11:36:52 -0300

    Implementação de consulta de documentos
    Consulta de documentos por data de vencimento utilizando query params

--- src/routes/document.route.js
+++ src/routes/document.route.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { createDocument, findAllDocuments, findDocumentById, updateDocument, deleteDocument } from "../controllers/document.controller.js";
+import { createDocument, findAllDocuments, findDocumentById, updateDocument, deleteDocument, findDocumentsWithParams } from "../controllers/document.controller.js";
 
 const documentRouter = Router();
 
 documentRouter.get('/', findAllDocuments);
+documentRouter.get('/data', findDocumentsWithParams);
 documentRouter.post('/', createDocument);
 documentRouter.get('/:id', findDocumentById);
 documentRouter.put('/:id', updateDocument);
